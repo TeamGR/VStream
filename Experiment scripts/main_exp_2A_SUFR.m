6e058cdf1c4cf0bf72c2e4a184eedad8d5af6346
Ommac <raffaello.camoriano@iit.it>
2014-06-09 20:57:22 -0400

    Updated esperiment version with signatures concatenation

--- Experiment scripts/main_exp_2A_SUFR.m
+++ Experiment scripts/main_exp_2A_SUFR.m
@@ -15,7 +15,7 @@ clear all;
 
 % Rotated images
 n_rotations = 7;
-Seq = cell(n_rotations);
+Seq = cell(n_rotations,1);
 imgPath = 'project\SUFRData\image_files\uniform_bg\roll';
 imgType = '*.jpg'; % change based on image type
 images  = dir([imgPath '\' imgType]);
@@ -26,7 +26,7 @@ rotated_images = Seq;
 
 % Translated Images
 n_translations = 5;
-Seq = cell(n_translations);
+Seq = cell(n_translations,1);
 imgPath = 'project\SUFRData\image_files\uniform_bg\translation';
 imgType = '*.jpg'; % change based on image type
 images  = dir([imgPath '\' imgType]);
@@ -37,7 +37,7 @@ translated_images = Seq;
 
 % Scaled images
 n_scales = 5;
-Seq = cell(n_scales);
+Seq = cell(n_scales,1);
 imgPath = 'project\SUFRData\image_files\uniform_bg\scaling';
 imgType = '*.jpg'; % change based on image type
 images  = dir([imgPath '\' imgType]);
@@ -69,18 +69,18 @@ n_bins = T.n_bins; % bars of the histogram
 range = [-1 1]; % range of the histogram
 
 % init data structures which will contain the output signatures
-S1transl = cell(n_translations);
-C1transl = cell(n_translations);
-S2transl = cell(n_translations);
-C2transl = cell(n_translations);
-S1rot = cell(n_rotations);
-C1rot = cell(n_rotations);
-S2rot = cell(n_rotations);
-C2rot = cell(n_rotations);
-S1scale = cell(n_scales);
-C1scale = cell(n_scales);
-S2scale = cell(n_scales);
-C2scale = cell(n_scales);
+S1transl = cell(n_translations,1);
+C1transl = cell(n_translations,1);
+S2transl = cell(n_translations,1);
+C2transl = cell(n_translations,1);
+S1rot = cell(n_rotations,1);
+C1rot = cell(n_rotations,1);
+S2rot = cell(n_rotations,1);
+C2rot = cell(n_rotations,1);
+S1scale = cell(n_scales,1);
+C1scale = cell(n_scales,1);
+S2scale = cell(n_scales,1);
+C2scale = cell(n_scales,1);
 
 %% S1 responses 
 
@@ -101,6 +101,7 @@ end
 for idx_rot=1:n_rotations
     histograms = poolingL1_giulia(S1rot{idx_rot}, n_splits, n_bins, range,  'histogram');
     signature = histograms(:, :, :, 1);
+    signature = signature(:);    
     normalized_signature = signature - mean(signature);
     normalized_signature = normalized_signature / norm(normalized_signature);
     C1rot{idx_rot} = normalized_signature;
@@ -118,6 +119,7 @@ end
 for idx_scale=1:n_scales
     histograms = poolingL1_giulia(S1scale{idx_scale}, n_splits, n_bins, range,  'histogram');
     signature = histograms(:, :, :, 1);
+    signature = signature(:);    
     normalized_signature = signature - mean(signature);
     normalized_signature = normalized_signature / norm(normalized_signature);
     C1scale{idx_scale} = normalized_signature;
@@ -153,3 +155,42 @@ end
 for idx_scale=1:n_scales
     C2scale{idx_scale} = poolingL2_giulia(S2scale{idx_scale}, n_splits, n_bins, range,  'histogram');
 end
+
+%% Output signatures concatenation
+
+C2tot = [];
+
+for ix_transl=1:n_translations
+    C2transl{idx_transl} = C2transl{idx_transl}(:, :, 1);
+    C2transl{idx_transl} = C2transl{idx_transl}(:);
+    C2tot = [ C2tot ; C2transl{idx_transl}]
+end
+
+for idx_rot=1:n_rotations
+    C2rot{idx_rot} = C2rot{idx_rot}(:, :, 1);
+    C2rot{idx_rot} = C2rot{idx_rot}(:);
+    C2tot = [ C2tot ; C2rot{idx_rot}]
+end
+
+for idx_scale=1:n_scales
+    C2scale{idx_scale} = C2scale{idx_scale}(:, :, 1);
+    C2scale{idx_scale} = C2scale{idx_scale}(:);
+    C2tot = [ C2tot ; C2scale{idx_scale}]
+end
+
+C2tot{idx_scale} = C2scale{idx_scale}(:, :, 1);
+    C2scale{idx_scale} = C2scale{idx_scale}(:);
+%% Response visualization and quantitative comaprison
+
+figure(2)
+%plot(signatures');
+%boxplot(signatures);
+
+m = mean(signatures);
+sd = std(signatures);
+f = [ m+2*sd , flipdim(m-2*sd,2)]; 
+fill([1:size(signatures,2) , size(signatures,2):-1:1] , f, [7 7 7]/8)
+hold on;
+plot(1:size(signatures,2) , m , 'b' , 'LineWidth',1);
+
+max(sd)
